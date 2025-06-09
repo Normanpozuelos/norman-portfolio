22fd1069372ce02e25f6239d3d86dbaeece1f5dd
Normanpozuelos <112310927+Normanpozuelos@users.noreply.github.com>
2025-06-09 12:18:04 +0200

    "navbar mobile menu"

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -11,14 +11,43 @@ export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
         <button
           onClick={() => setMenuOpen(false)}
           className="absolute top-6 right-6 text-white text-3xl cursor-pointer z-50"
+          aria-label="Close menu"
         >
           &times;
         </button>
-  
+        <a href="#home" 
+           onClick={() => setMenuOpen(false)}
+           className={`text-2xl font-semibold text-white my-4 transform transition-transform
+                       duration-300 
+                       ${menuOpen 
+                       ? "opacity-100 translate-y-0" 
+                       : "opacity-0"} `}>Home</a>
+        <a href="#about" 
+            onClick={() => setMenuOpen(false)}
+            className={`text-2xl font-semibold text-white my-4 transform transition-transform
+              duration-300 
+              ${menuOpen 
+              ? "opacity-100 translate-y-0" 
+              : "opacity-0"} `}
+           >About</a>
+        <a href="#projects" 
+            onClick={() => setMenuOpen(false)}
+            className={`text-2xl font-semibold text-white my-4 transform transition-transform
+              duration-300 
+              ${menuOpen 
+              ? "opacity-100 translate-y-0" 
+              : "opacity-0"} `}>Projects</a>
+        <a href="#contact" 
+            onClick={() => setMenuOpen(false)}
+            className={`text-2xl font-semibold text-white my-4 transform transition-transform
+              duration-300 
+              ${menuOpen 
+              ? "opacity-100 translate-y-0" 
+              : "opacity-0"} `}>Contact</a>        
         {/* Optional: Menu content */}
         <div className="text-white text-xl mt-4">Mobile Menu</div>
       </div>
     );
   };
   
-//39 not working x button
+//
